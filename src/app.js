b172bb7f74ff744119b1460dfc6e9436becc0038
Dayeon Won <aakk9350@naver.com>
2021-10-26 12:18:03 +0900

    prettier 실습

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 /**
- * TODO: Prettier가 스타일을 수정합니다.
+ * TODO eslint가 중복 세미콜론을 제거합니다.
  */
 
+import MainController from "./controllers/MainController.js";
 
-import MainController from './controllers/MainController.js'
+import "./app.scss";
 
-import './app.scss'
+// const foo = ''
 
-
-document.addEventListener("DOMContentLoaded", ()=>{
-new MainController(); 
-})
+document.addEventListener("DOMContentLoaded", () => {
+  new MainController();
+});
